56dfbfc4cc2055f718d632dc58012ccf8cfb6fd4
landiluigi746 <landiluigi865@gmail.com>
2026-08-28 20:55:25 +0200

    [docs, wip]: Document assert header

--- include/cstl/assert.h
+++ include/cstl/assert.h
@@ -4,11 +4,23 @@
 void assert_msg(int expr, const char* exprMsg, const char* msg);
 
 #if defined(CSTL_DEBUG)
+/**
+ * @brief Macro for debugging assertions, enabled only in debug mode of cstl.
+ *
+ * @param expr The expression to assert.
+ * @param msg The error message to display if the assertion fails.
+ */
 #define CSTL_ASSERT_DEBUG(expr, msg) assert_msg((expr), (#expr), (msg))
 #else
 #define CSTL_ASSERT_DEBUG(expr)
 #endif
 
+/**
+ * @brief Macro for assertions.
+ *
+ * @param expr The expression to assert.
+ * @param msg The error message to display if the assertion fails.
+ */
 #define CSTL_ASSERT(expr, msg) assert_msg((expr), (#expr), (msg))
 
 #endif
