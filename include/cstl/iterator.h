9026861d84ed14a261b82e95bb7cd01e72b6e5ac
landiluigi746 <landiluigi865@gmail.com>
2026-09-03 02:26:45 +0200

    [feat, wip]: Begin implementation of iterator module

---
+++ include/cstl/iterator.h
@@ -0,0 +1,84 @@
+#ifndef CSTL_ITERATOR_H
+#define CSTL_ITERATOR_H
+
+#include <stddef.h>
+
+/**
+ * @brief An iterator represents a position in a container and provides functions to traverse it.
+ */
+typedef struct iterator iterator_t;
+
+/**
+ * @brief Alias for a function pointer used to move an iterator to the next position.
+ */
+typedef iterator_t (*next_fn)(iterator_t);
+
+/**
+ * @brief Alias for a function pointer used to move an iterator to the previous position.
+ */
+typedef iterator_t (*prev_fn)(iterator_t);
+
+/**
+ * @brief Alias for a function pointer used to get the value at an iterator's position.
+ */
+typedef void* (*get_fn)(iterator_t);
+
+/**
+ * @brief Alias for a function pointer used to compare two iterators.
+ */
+typedef int (*cmp_fn)(iterator_t, iterator_t);
+
+/**
+ * @brief Struct holding function pointers for iterator operations.
+ */
+typedef struct iterator_funcs
+{
+    next_fn next;
+    prev_fn prev;
+    get_fn get;
+    cmp_fn cmp;
+} iterator_funcs_t;
+
+/**
+ * @brief The underlying struct representing an iterator.
+ */
+struct iterator
+{
+    void* context;
+    void* pointer;
+    const iterator_funcs_t* funcs;
+};
+
+/**
+ * @brief Moves the iterator to the next position.
+ *
+ * @param it The iterator to move.
+ */
+iterator_t iterator_next(iterator_t it);
+
+/**
+ * @brief Moves the iterator to the previous position.
+ *
+ * @param it The iterator to move.
+ */
+iterator_t iterator_prev(iterator_t it);
+
+/**
+ * @brief Gets the value at the iterator's current position.
+ *
+ * @param it The iterator to get the value from.
+ * @return The value at the iterator's current position.
+ */
+void* iterator_get(iterator_t it);
+
+/**
+ * @brief Compares two iterators.
+ *
+ * @param it1 The first iterator to compare.
+ * @param it2 The second iterator to compare.
+ * @return 0 if the iterators are equal, a negative value if it1 is less than it2, or a positive value if it1 is
+ * greater than it2.
+ */
+int iterator_cmp(iterator_t a, iterator_t b);
+
+#endif
---
+++ src/iterator.c
@@ -0,0 +1,32 @@
+#include "cstl/iterator.h"
+
+#include "cstl/assert.h"
+
+iterator_t iterator_next(iterator_t it)
+{
+    CSTL_ASSERT_DEBUG(it.funcs != NULL, "Can't move an iterator without operations to next position");
+    CSTL_ASSERT_DEBUG(it.funcs->next != NULL, "Can't move a non-forward iterator to next position");
+    return it.funcs->next(it);
+}
+
+iterator_t iterator_prev(iterator_t it)
+{
+    CSTL_ASSERT_DEBUG(it.funcs != NULL, "Can't move an iterator without operations to prev position");
+    CSTL_ASSERT_DEBUG(it.funcs->prev != NULL, "Can't move a non-bidirectional iterator to prev position");
+    return it.funcs->prev(it);
+}
+
+void* iterator_get(iterator_t it)
+{
+    CSTL_ASSERT_DEBUG(it.funcs != NULL, "Can't get pointer from an iterator without operations");
+    CSTL_ASSERT_DEBUG(it.funcs->get != NULL, "Can't get pointer from an iterator without get operation");
+    return it.funcs->get(it);
+}
+
+int iterator_cmp(iterator_t a, iterator_t b)
+{
+    CSTL_ASSERT_DEBUG(a.funcs != NULL && b.funcs != NULL, "Can't compare iterators without operations");
+    CSTL_ASSERT_DEBUG(a.funcs->cmp != NULL && a.funcs->cmp == b.funcs->cmp,
+                      "Can't compare iterators with different comparison functions");
+    return a.funcs->cmp(a, b);
+}
