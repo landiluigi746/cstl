af1b148283d832c97374484aed6056779794dfbc
landiluigi746 <landiluigi865@gmail.com>
2026-09-07 00:33:03 +0200

    [fix]: Add missing includes in cstl.h

--- include/cstl/cstl.h
+++ include/cstl/cstl.h
@@ -1,7 +1,9 @@
 #ifndef CSTL_H
 #define CSTL_H
 
+#include "cstl/algorithm.h"
 #include "cstl/assert.h"
+#include "cstl/iterator.h"
 #include "cstl/memory.h"
 #include "cstl/shared_ptr.h"
 #include "cstl/string.h"
